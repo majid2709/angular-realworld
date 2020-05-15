aedb16e417610eba8f361a8efeafb11418a412ff
majid2709 <63237287+majid2709@users.noreply.github.com>
2020-05-15 13:45:44 +0530

    Updated ami_script

--- scripts/ami-creation.sh
+++ scripts/ami-creation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-a=$(date "+%Y%m%d")
+a=$(date "+%Y%m%d-%H:%M:%S")
 aws ec2 create-image --instance-id i-090ffb35156dbd4eb --name "My server_$a" --description "An AMI for my server" --no-reboot
