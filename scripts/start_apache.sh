361a225e05fe16f9bd13c1eb1817542b03097a7c
majid2709 <63237287+majid2709@users.noreply.github.com>
2020-05-17 01:33:58 +0530

    Updated start_apache

--- scripts/start_apache.sh
+++ scripts/start_apache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 a=$(date "+%Y%m%d%H%M%S")
-service apache2 start
+service apache2 start;
 aws ec2 create-image --instance-id i-090ffb35156dbd4eb --name "My server_$a" --description "An AMI for my server" --no-reboot
